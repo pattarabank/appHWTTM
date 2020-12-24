0b6383771cd6fc465f495129ccb59408b2f405b8
pattarabank <l3elievelzz@gmail.com>
2020-12-24 11:18:24 +0700

    24/12/2020 11:18 AM

--- app/src/main/java/com/example/apphwttm/MainActivity.kt
+++ app/src/main/java/com/example/apphwttm/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
+    //test git commit 24/12/2020
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
