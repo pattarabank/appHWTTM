01b7e421c8d53f2e6ca02aae731cbd408336fb04
pattarabank <l3elievelzz@gmail.com>
2021-02-02 16:37:26 +0700

    2/2/2021 4:36 PM user info page

--- app/src/main/java/com/example/apphwttm/RegisterActivity.kt
+++ app/src/main/java/com/example/apphwttm/RegisterActivity.kt
@@ -1,11 +1,40 @@
 package com.example.apphwttm
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.get
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterActivity : AppCompatActivity() {
+
+//    val sharedPreferencesName = getSharedPreferences("sharedPrefsName", Context.MODE_PRIVATE)
+//    val editorName = sharedPreferencesName.edit()
+//    editorName.apply {
+//        putString("sharedPrefsName", name)
+//    }.apply()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val outlinedButton: Button = findViewById(R.id.outlinedButton)
+        val nameTextField: TextInputLayout = findViewById(R.id.nameTextField)
+        val birthDateTextField: TextInputLayout = findViewById(R.id.birthDateTextField)
+        val diseaseTextField: TextInputLayout = findViewById(R.id.diseaseTextField)
+
+        outlinedButton.setOnClickListener {
+            var name = nameTextField.editText?.text.toString()
+            var bDate = birthDateTextField.editText?.text.toString()
+            var disease = diseaseTextField.editText?.text.toString()
+            //Snackbar.make(it, "$name $bDate $disease", Snackbar.LENGTH_SHORT).show()
+        }
+
+
     }
 }
--- app/src/main/java/com/example/apphwttm/SplashScreen.kt
+++ app/src/main/java/com/example/apphwttm/SplashScreen.kt
@@ -1,10 +1,12 @@
 package com.example.apphwttm
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,12 +27,27 @@ class SplashScreen : AppCompatActivity() {
 //        }, 3000)
 
         //test
-        Handler(Looper.getMainLooper()).postDelayed({
-            // Your Code
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+        val sharedPreferences = getSharedPreferences("sharedPrefsName", Context.MODE_PRIVATE)
+//        val editor = sharedPreferences.edit()
+//        editor.apply {
+//            putString("sharedPrefsName", "-")
+//        }.apply()
 
+        val isBlank = sharedPreferences.getString("sharedPrefsName", null)
+        Log.d("TEST_SP", isBlank.toString())
+        if (isBlank == null) {
+            Handler(Looper.getMainLooper()).postDelayed({
+                // Your Code
+                val intent = Intent(this, RegisterActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000)
+        } else {
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000)
+        }
     }
 }
