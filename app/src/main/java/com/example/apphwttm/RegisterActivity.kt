bd03acac1ef34bd12f20a680dd81daa5fd5e993c
pattarabank <l3elievelzz@gmail.com>
2021-03-30 05:44:50 +0700

    userInfo page rework

--- app/src/main/java/com/example/apphwttm/RegisterActivity.kt
+++ app/src/main/java/com/example/apphwttm/RegisterActivity.kt
@@ -6,60 +6,96 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.core.view.get
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.whiteelephant.monthpicker.MonthPickerDialog
+import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
 
-//    val sharedPreferencesName = getSharedPreferences("sharedPrefsName", Context.MODE_PRIVATE)
+    //    val sharedPreferencesName = getSharedPreferences("sharedPrefsName", Context.MODE_PRIVATE)
 //    val editorName = sharedPreferencesName.edit()
 //    editorName.apply {
 //        putString("sharedPrefsName", name)
 //    }.apply()
 
+    private lateinit var regisYearBtn: ExtendedFloatingActionButton
+    private lateinit var myCalendar: TextView
+    private lateinit var user_year_txt : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
-        val outlinedButton: Button = findViewById(R.id.outlinedButton)
-        val nameTextField: TextInputLayout = findViewById(R.id.nameTextField)
-        val birthDateTextField: TextInputLayout = findViewById(R.id.birthDateTextField)
-        val diseaseTextField: TextInputLayout = findViewById(R.id.diseaseTextField)
-
-        outlinedButton.setOnClickListener {
-            var name = nameTextField.editText?.text.toString()
-            val sharedPreferencesName = getSharedPreferences("sharedPrefsName", Context.MODE_PRIVATE)
-            val editorName = sharedPreferencesName.edit()
-            editorName.apply {
-                putString("sharedPrefsName", name)
-            }.apply()
-
-
-            var bDate = birthDateTextField.editText?.text.toString()
-            val sharedPreferencesBDate = getSharedPreferences("sharedPrefs_bDate", Context.MODE_PRIVATE)
-            val editorBDate = sharedPreferencesBDate.edit()
-            editorBDate.apply {
-                putString("sharedPrefs_bDate", bDate)
-            }.apply()
-
-
-            var disease = diseaseTextField.editText?.text.toString()
-            val sharedPreferencesDisease = getSharedPreferences("sharedPrefsDisease", Context.MODE_PRIVATE)
-            val editorDisease = sharedPreferencesDisease.edit()
-            editorDisease.apply {
-                putString("sharedPrefsDisease", disease)
-            }.apply()
-
-            //Snackbar.make(it, "$name $bDate $disease", Snackbar.LENGTH_SHORT).show()
-
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-
+        myCalendar = findViewById(R.id.regis_txt_calendar)
+        myCalendar.setOnClickListener {
+            btnYear()
         }
+        regisYearBtn = findViewById(R.id.extended_fab)
+        regisYearBtn.setOnClickListener {
+            //set data to sharedpreference
+        }
+        user_year_txt = findViewById(R.id.user_year_txt)
+//        val outlinedButton: Button = findViewById(R.id.outlinedButton)
+//        val nameTextField: TextInputLayout = findViewById(R.id.nameTextField)
+//        val birthDateTextField: TextInputLayout = findViewById(R.id.birthDateTextField)
+//
+//        outlinedButton.setOnClickListener {
+//            var name = nameTextField.editText?.text.toString()
+//            val sharedPreferencesName = getSharedPreferences("sharedPrefsName", Context.MODE_PRIVATE)
+//            val editorName = sharedPreferencesName.edit()
+//            editorName.apply {
+//                putString("sharedPrefsName", name)
+//            }.apply()
+//
+//
+//            var bDate = birthDateTextField.editText?.text.toString()
+//            val sharedPreferencesBDate = getSharedPreferences("sharedPrefs_bDate", Context.MODE_PRIVATE)
+//            val editorBDate = sharedPreferencesBDate.edit()
+//            editorBDate.apply {
+//                putString("sharedPrefs_bDate", bDate)
+//            }.apply()
+//
+//
+//
+//            //Snackbar.make(it, "$name $bDate $disease", Snackbar.LENGTH_SHORT).show()
+//
+//            val intent = Intent(this, HomeActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//
+//        }
 
 
     }
+
+    private fun btnYear() {
+        var today: Calendar = Calendar.getInstance()
+        var builder: MonthPickerDialog.Builder = MonthPickerDialog.Builder(
+            this@RegisterActivity,
+            { selectedMonth, selectedYear ->
+                run {
+                    user_year_txt.text = selectedYear.toString()
+                }
+            },
+            today.get(Calendar.YEAR),
+            today.get(Calendar.MONTH)
+        )
+        builder.setActivatedMonth(Calendar.JANUARY)
+            .setMinYear(1990)
+            .setActivatedYear(today.get(Calendar.YEAR))
+            .setMaxYear(2030)
+            .setTitle("SELECT YEAR")
+            .showYearOnly()
+            .build().show()
+    }
 }
+//            var disease = diseaseTextField.editText?.text.toString()
+//            val sharedPreferencesDisease = getSharedPreferences("sharedPrefsDisease", Context.MODE_PRIVATE)
+//            val editorDisease = sharedPreferencesDisease.edit()
+//            editorDisease.apply {
+//                putString("sharedPrefsDisease", disease)
+//            }.apply()
