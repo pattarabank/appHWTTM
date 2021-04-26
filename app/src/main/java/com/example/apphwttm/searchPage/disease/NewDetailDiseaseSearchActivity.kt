331e6d91da3ed70618f2abb5362a78b3b551b70f
pattarabank <l3elievelzz@gmail.com>
2021-04-27 00:51:49 +0700

    remove fragment,viewpager use onTabSelected instead

--- app/src/main/java/com/example/apphwttm/searchPage/disease/NewDetailDiseaseSearchActivity.kt
+++ app/src/main/java/com/example/apphwttm/searchPage/disease/NewDetailDiseaseSearchActivity.kt
@@ -1,24 +1,17 @@
 package com.example.apphwttm.searchPage.disease
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
-import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
 import com.example.apphwttm.R
-import com.example.apphwttm.searchPage.disease.fragments.NewDetailDisease1Fragment
-import com.example.apphwttm.searchPage.disease.fragments.NewDetailDisease2Fragment
-import com.example.apphwttm.searchPage.disease.fragments.ViewPagerNewDetailAdapter
 import com.google.android.material.tabs.TabLayout
 
 class NewDetailDiseaseSearchActivity : AppCompatActivity() {
 
-    private lateinit var viewPager: ViewPager
-    private lateinit var tabsLayout: TabLayout
+    private lateinit var tabsLayout2: TabLayout
 
     private lateinit var textView1: TextView
+    private lateinit var testTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,34 +20,40 @@ class NewDetailDiseaseSearchActivity : AppCompatActivity() {
         var bundle: Bundle? = intent.extras
         var detail_disease_name = bundle!!.getString("send_to_detail_disease_name")
         var detail_disease_des = bundle!!.getString("send_to_detail_disease_des")
-
-        Log.d("TEST_STR", detail_disease_des.toString())
-
+        //Log.d("TEST_STR", detail_disease_des.toString())
         var detail_disease_des_kid = bundle!!.getString("send_to_detail_disease_des_kid")
-
-        Log.d("TEST_STR", detail_disease_des_kid.toString())
+        //Log.d("TEST_STR", detail_disease_des_kid.toString())
 
         textView1 = findViewById(R.id.textViewNewDisease)
         textView1.text = detail_disease_name
 
+        //test
+        testTextView = findViewById(R.id.test_textview)
+        testTextView.text = detail_disease_des
+        tabsLayout2 = findViewById(R.id.tabLayout2)
+        tabsLayout2.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                //Log.d("TESTTAG",tab.toString())
+                if (tab?.position == 0) {
+                    //Log.d("TESTTAG","TAB1")
+                    testTextView.text = detail_disease_des
+                } else {
+                    //Log.d("TESTTAG","TAB2")
+                    testTextView.text = detail_disease_des_kid
+                }
+            }
 
-        setUpTabs()
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
 
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
 
     }
-
-    private fun setUpTabs() {
-        val adapter = ViewPagerNewDetailAdapter(supportFragmentManager)
-        adapter.addFragment(NewDetailDisease1Fragment(), "ผู้ใหญ่")
-        adapter.addFragment(NewDetailDisease2Fragment(), "เด็ก")
-        viewPager = findViewById(R.id.viewPager)
-        viewPager.adapter = adapter
-        tabsLayout = findViewById(R.id.tabLayout)
-        tabsLayout.setupWithViewPager(viewPager)
-
-
-    }
-
 
 
 }
