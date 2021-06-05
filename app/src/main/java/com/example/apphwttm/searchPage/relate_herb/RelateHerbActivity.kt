f1a01b6935b0059005e1ce78fc75c87b0bb4d747
pattarabank <l3elievelzz@gmail.com>
2021-06-05 14:50:19 +0700

    update relateherbactivity

--- app/src/main/java/com/example/apphwttm/searchPage/relate_herb/RelateHerbActivity.kt
+++ app/src/main/java/com/example/apphwttm/searchPage/relate_herb/RelateHerbActivity.kt
@@ -21,39 +21,67 @@ class RelateHerbActivity : AppCompatActivity() {
     private var searchListHerb: List<HerbSearchModel> = ArrayList()
     private val searchListHerbAdapter = SearchListHerbAdapter(searchListHerb)
 
+    private var myList: MutableList<String> = ArrayList<String>()
+    private var searchListName: MutableList<String> = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relate_herb)
 
+
         val bundle: Bundle? = intent.extras
         val detail_des: String = bundle!!.getString("disease_des").toString()
         val detail_des_kid: String = bundle!!.getString("disease_des_kid").toString()
-        Log.d("TEST_RELATE_HERB", detail_des.toString())
-        Log.d("TEST_RELATE_HERB", detail_des_kid.toString())
+        //Log.d("TEST_RELATE_HERB", detail_des.toString())
+        //Log.d("TEST_RELATE_HERB", detail_des_kid.toString())
+        val strLine = detail_des + "" + detail_des_kid
+        strLine.filter { !it.isWhitespace() }.trim()
+        //Log.d("TEST_RELATE_HERB", strLine)
 
+        val searchListHerbRecyclerView =
+            findViewById<RecyclerView>(R.id.search_RecyclerView_relate_herb)
+        searchListHerbRecyclerView.hasFixedSize()
+        searchListHerbRecyclerView.layoutManager = LinearLayoutManager(this)
+        searchListHerbRecyclerView.adapter = searchListHerbAdapter
 
-        val searchListHerb = findViewById<RecyclerView>(R.id.search_RecyclerView_relate_herb)
-        searchListHerb.hasFixedSize()
-        searchListHerb.layoutManager = LinearLayoutManager(this)
-        searchListHerb.adapter = searchListHerbAdapter
+        searchInFirestore(strLine)
 
-        searchInFirestore()
 
     }
 
-    private fun searchInFirestore() {
-        firebaseFirestore.collection("herb")
-            //.whereArrayContains("keyword", searchText)
-            .get().addOnCompleteListener {
-                if (it.isSuccessful) {
-                    searchListHerb = it.result!!.toObjects(HerbSearchModel::class.java)
-                    searchListHerbAdapter.herbSearchModelList = searchListHerb
-                    searchListHerbAdapter.notifyDataSetChanged()
-                } else {
-                    Log.d(TAG, "Error: ${it.exception!!.message}")
+    private fun searchInFirestore(lineOfDes: String) {
+
+
+        firebaseFirestore.collection("herb").get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                for (document in task.result!!) {
+                    var name = document.getString("name")
+                    myList.add(name.toString())
                 }
             }
+            Log.d("TEST_RELATE_HERB", myList.toString())
+            myList.forEach { str ->
+                if (lineOfDes.contains(str)) {
+                    searchListName.add(str)
+                }
+            }
+            Log.d("TEST_RELATE_HERB", searchListName.toString())
+            firebaseFirestore.collection("herb")
+                .whereArrayContainsAny("keyword", searchListName)
+                .get().addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        searchListHerb = it.result!!.toObjects(HerbSearchModel::class.java)
+                        searchListHerbAdapter.herbSearchModelList = searchListHerb
+                        searchListHerbAdapter.notifyDataSetChanged()
+                    } else {
+                        Log.d(TAG, "Error: ${it.exception!!.message}")
+                    }
+                }
+
+        }
+
+
     }
+
 
     override fun onStart() {
         super.onStart()
